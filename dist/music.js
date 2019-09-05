b17d9f3b6d11477451074a1f35da517c75f73ec1
itzhanglg <1321340821@qq.com>
2019-09-06 02:03:43 +0800

    Site updated: 2019-09-06 02:03:32

---
+++ dist/music.js
@@ -0,0 +1,61 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "来自天堂的魔鬼",
+        artist: '邓紫棋',
+        url: 'http://www.ytmp3.cn/down/33626.mp3',
+        cover: 'http://img.ytmp3.cn/image/82.jpg',
+      },
+      {
+        name: '一曲相思',
+        artist: '半阳',
+        url: 'http://www.ytmp3.cn/down/47681.mp3',
+        cover: 'http://img.ytmp3.cn/image/91.jpg',
+      },
+      {
+        name: '风筝误',
+        artist: '刘珂矣',
+        url: 'http://up.mcyt.net/?down/46644.mp3',
+        cover: 'http://img.ytmp3.cn/image/54.jpg',
+      },
+	  {
+        name: '烦恼歌',
+        artist: '张学友',
+        url: 'http://www.ytmp3.cn/down/58245.mp3',
+        cover: 'http://img.ytmp3.cn/image/12.jpg',
+      },
+	  {
+        name: '琵琶行',
+        artist: '沈谧仁&奇然',
+        url: 'http://www.ytmp3.cn/down/57337.mp3',
+        cover: 'http://img.ytmp3.cn/image/77.jpg',
+      },
+	  {
+        name: '回忆总想哭',
+        artist: '南宫嘉骏',
+        url: 'http://www.ytmp3.cn/down/43051.mp3',
+        cover: 'http://img.ytmp3.cn/image/8.jpg',
+      },
+	  {
+        name: '带你去旅行',
+        artist: '校长',
+        url: 'http://www.ytmp3.cn/down/43135.mp3',
+        cover: 'http://img.ytmp3.cn/image/46.jpg',
+      },
+	  {
+        name: '温柔乡',
+        artist: '陈雅森',
+        url: 'http://www.ytmp3.cn/down/38593.mp3',
+        cover: 'http://img.ytmp3.cn/image/55.jpg',
+      },
+	  {
+        name: '林中鸟',
+        artist: '葛林',
+        url: 'http://www.ytmp3.cn/down/46333.mp3',
+        cover: 'http://img.ytmp3.cn/image/8.jpg',
+      },
+    ]
+});
