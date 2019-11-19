0c841c176fd64344e4cba99e12d3598360a18655
itzhanglg <1321340821@qq.com>
2019-11-20 00:22:12 +0800

    Site updated: 2019-11-20 00:21:58

--- dist/music.js
+++ dist/music.js
@@ -3,59 +3,78 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-      /*{
-        name: "来自天堂的魔鬼",
-        artist: '邓紫棋',
-        url: 'http://www.ytmp3.cn/down/33626.mp3',
-        cover: 'http://img.ytmp3.cn/image/82.jpg',
+      {
+        name: '你是人间四月天',
+        artist: '解忧邵帅',
+        url: 'http://music.163.com/song/media/outer/url?id=1344897943.mp3',
+        cover: 'http://p1.music.126.net/pjuhmIbOS3t6T87xgq7t8g==/109951164450922616.jpg?param=180y180',
       },
       {
-        name: '一曲相思',
-        artist: '半阳',
-        url: 'http://www.ytmp3.cn/down/57306.mp3',
-        cover: 'http://img.ytmp3.cn/image/91.jpg',
+        name: '林中鸟',
+        artist: '葛林',
+        url: 'https://sharefs.yun.kugou.com/201911200004/7e0ef1fcd6db7a2c7687402c3d7bc774/G054/M09/19/11/dg0DAFbV-miADoliADicM-lnciU201.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20190909/20190909200303910780.jpg',
       },
       {
         name: '风筝误',
         artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://img.ytmp3.cn/image/54.jpg',
+        url: 'https://sharefs.yun.kugou.com/201911192359/c96d9864f37989f8f565a600f3ccdde0/G022/M06/12/03/9pMEAFWKLteAftb0AD8A_yIk33g406.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20190916/20190916165156913.jpg',
       },
-	  {
-        name: '烦恼歌',
-        artist: '张学友',
-        url: 'http://www.ytmp3.cn/down/58245.mp3',
-        cover: 'http://img.ytmp3.cn/image/12.jpg',
+      {
+        name: "把孤独当做晚餐",
+        artist: '井胧',
+        url: 'https://sharefs.yun.kugou.com/201911200002/1317b5fe26a552b5f355d54a74ef12a4/G166/M07/0D/19/hpQEAF1Lu_yAPR7pACrtj4GEOqE154.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20190802/20190802180102888.jpg',
       },
-	  {
-        name: '琵琶行',
-        artist: '沈谧仁&奇然',
-        url: 'http://www.ytmp3.cn/down/57337.mp3',
-        cover: 'http://img.ytmp3.cn/image/77.jpg',
-      },
-	  {
+      {
         name: '回忆总想哭',
         artist: '南宫嘉骏',
-        url: 'http://www.ytmp3.cn/down/43051.mp3',
-        cover: 'http://img.ytmp3.cn/image/8.jpg',
+        url: 'https://sharefs.yun.kugou.com/201911200002/ac8b495ddb9d432fcb0a052d0370c10c/G123/M09/08/09/W5QEAFrZpESAajT1AEcQLWoJFaU581.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20190815/20190815162602712.jpg',
       },
-	  {
+      {
+        name: "芒种",
+        artist: '音阙诗听',
+        url: 'https://sharefs.yun.kugou.com/201911200007/f275c8ec4d285d3ce37c2d4fbb216fed/G153/M04/13/14/OYcBAFz3fF6AbF0fADS_2OPt0ag626.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20170721/20170721162945806.jpg',
+      },
+      {
+        name: "来自天堂的魔鬼",
+        artist: '邓紫棋',
+        url: 'https://sharefs.yun.kugou.com/201911192346/3f9c8fb495b5d9f9b660368f00eefdc4/G108/M00/0D/0D/DIcBAFvgHyOAUoSLADv7TtiRGW8736.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20151111/20151111174428152608.jpg',
+      },
+      {
+        name: '一曲相思',
+        artist: '半阳',
+        url: 'http://music.163.com/song/media/outer/url?id=1313558186.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20181116/20181116190719480.jpg',
+      },
+	    {
+        name: '烦恼歌',
+        artist: '张学友',
+        url: 'https://sharefs.yun.kugou.com/201911200000/9c367dc970aaf102cfbe2ee082b824c8/G008/M08/0E/17/qIYBAFUMMP6AeMr3AD5AA-h75Jw415.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20140527/20140527095042283066.jpg',
+      },
+	    {
+        name: '琵琶行',
+        artist: '沈谧仁&奇然',
+        url: 'https://sharefs.yun.kugou.com/201911200001/412b7e1bc1b483a09a2508a2d3709256/G101/M04/03/1B/RZQEAFkIoriANtRPAFHc6NdLKWM590.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20161028/20161028095734731.jpg',
+      },
+	    {
         name: '带你去旅行',
         artist: '校长',
-        url: 'http://www.ytmp3.cn/down/43135.mp3',
-        cover: 'http://img.ytmp3.cn/image/46.jpg',
+        url: 'https://sharefs.yun.kugou.com/201911200003/0668005bc708e8e0f70ca0544bb195f3/G108/M0B/10/1D/rA0DAFmNMtuAUhesADcks-2d4EQ165.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20190624/20190624181655780.jpg',
       },
-	  {
+	    {
         name: '温柔乡',
         artist: '陈雅森',
-        url: 'http://www.ytmp3.cn/down/38593.mp3',
-        cover: 'http://img.ytmp3.cn/image/55.jpg',
+        url: 'https://sharefs.yun.kugou.com/201911200004/53274355ff48966598b062b899f5e100/G027/M04/05/13/Ww0DAFWeMRuAS1HIAD3r6i-KjiE901.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/softhead/240/20190701/20190701102756737.jpg',
       },
-	  {
-        name: '林中鸟',
-        artist: '葛林',
-        url: 'http://www.ytmp3.cn/down/46333.mp3',
-        cover: 'http://img.ytmp3.cn/image/8.jpg',
-      },*/
+
     ]
 });
